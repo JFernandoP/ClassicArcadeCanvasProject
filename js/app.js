4a59b8f6ea1fc6ddea7f5bc229a4f44887d0fa20
JFernandoP <jf_padilla@yahoo.com>
2018-09-19 17:01:40 -0400

    test: define missing code to enable render
    define enemy and player classes
    define enemy and player variables

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+function Enemy() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = 101;
+    this.y = 83;
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +27,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+function Player () {
+  this.sprite = 'images/char-boy.png'
 
+  this.x = 202;
+  this.y = 166;
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var enemy1 = new Enemy();
+var allEnemies = [enemy1];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
