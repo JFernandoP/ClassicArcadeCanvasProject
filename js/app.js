8033f965cb9f76d5871347e35ffe15d04bde6fe5
JFernandoP <jf_padilla@yahoo.com>
2018-09-19 20:42:24 -0400

    check: enable enemy motion
    - establish character image positions on game tile grid
    - apply a velocity to enemy character

--- js/app.js
+++ js/app.js
@@ -8,7 +8,22 @@ function Enemy() {
     this.sprite = 'images/enemy-bug.png';
 
     this.x = 101;
-    this.y = 83;
+    this.y = 64;
+// The height of the opaque part of a ground tile is effectively given by:
+//   171 - 50 = 121
+// By inspection, the enemy image is centered about this opaque part.
+// Center the enemy image about the lit part of the ground.
+// The height of the lit part of a ground is effectively 83.
+// So, the position shift to center enemy image to this lit part is:
+//   83/2 - 121/2 = -38/2 = -19
+// Note that 38 = 121 - 83 = the height of the unlit part of the ground.
+// Thus, enemy image position at row 0 is: 0 - 19 = -19.
+// Enemy image position at row 1 is: 83 - 19 = 64.
+// . . .
+// Enemy image position at row n is: n*83 - 19.
+// That is, this.y(row) = row*83 - 19.
+
+    this.vx = 100; // enemy velocity
 };
 
 // Update the enemy's position, required method for game
@@ -17,6 +32,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.vx*dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,10 +44,12 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 function Player () {
-  this.sprite = 'images/char-boy.png'
+  this.sprite = 'images/char-boy.png';
 
   this.x = 202;
-  this.y = 166;
+  this.y = 166-19;
+  // By choice, employ same sprite shift as Enemy:
+  // this.y(row) = row*83 - 19.
 };
 
 Player.prototype.update = function(dt) {
